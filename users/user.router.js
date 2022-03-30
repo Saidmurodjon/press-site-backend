fd4f5cb645629b01513366c3089e09f4d05191a3
kali <kali@kali.kali>
2022-03-30 08:07:45 +0500

    updated

--- users/user.router.js
+++ users/user.router.js
@@ -3,10 +3,10 @@ const router =express.Router()
 
 const user=require('./user')
 router.route('/').get(user.getUser)
-// router.route('/').post(uploadMulter,user.addPupils)
-// router.route('/:id').put(uploadMulter,user.updatePupils)
-// router.route('/:id').delete(user.deletePupils)
-// router.route('/pay/:id').put(user.payPupils)
+// router.route('/').post(user.addUser)
+// router.route('/:id').put(user.updateUser)
+// router.route('/:id').delete(user.deleteUser)
+// router.route('/pay/:id').put(user.payUser)
 
 
 
