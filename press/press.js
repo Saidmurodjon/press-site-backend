58d8dfe5a8178a95755ed0ead6764112a4ce1723
kali <kali@kali.kali>
2022-04-11 23:24:27 +0500

    update some functions

--- press/press.js
+++ press/press.js
@@ -1,68 +1,66 @@
-const PressModel=require('./press.model')
-const fs = require('fs');
-const path = require('path');
+const PressModel = require("./press.model");
+const fs = require("fs");
+const path = require("path");
 
-async function getPress(req,res) {
-    try{
-        const press=await PressModel.find({})
-        return res.status(200).send(press)
-    }catch(err){
-        res.status(400).send(err)
-        console.log(err);
-    }
+async function getPress(req, res) {
+  try {
+    const press = await PressModel.find({});
+    return res.status(200).send(press);
+  } catch (err) {
+    res.status(400).send(err);
+    console.log(err);
+  }
 }
 
+async function addPress(req, res) {
+  try {
+    // let image =req.file.originalname
+    // let userId=req.params.id
 
-async function addPress(req,res) {
-    try{
-        // let image =req.file.originalname
-        // let userId=req.params.id
+    const category = new PressModel({
+      userId: req.body.userId,
+      title: req.body.title,
+      genre: req.body.genre,
+      tags: req.body.tags,
+      theme: req.body.theme,
+      text: req.body.text,
+      view: req.body.view,
+      commit: req.body.commit,
+    });
 
-        const category = new PressModel({
-            
-            userId:req.body.userId,
-            title:req.body.title,
-            genre:req.body.genre,
-            tags:req.body.tags,
-            theme:req.body.theme,
-            text:req.body.text,
-            view: req.body.view,
-            commit: req.body.commit,
-        })
-      
-        category.save((err, category) => {
-            if (err) {
-                console.log(err)
-                return res.status(400).json({
-                    errors: err.meesage
-                })
-            }
-            return res.json({
-                message: "Created category successfully",
-                category
-            })
-        })
-    }catch(err){
-        res.status(400).send(err)
-    }
+    category.save((err, category) => {
+      if (err) {
+        console.log(err);
+        return res.status(400).json({
+          errors: err.meesage,
+        });
+      }
+      return res.json({
+        message: "Created category successfully",
+        category,
+      });
+    });
+  } catch (err) {
+    res.status(400).send(err);
+  }
 }
 
 // async function updatePupils(req,res) {
 //     try{
 //         let userId=req.params.id
 //         let del= await PupilsModel.findOne({userId})
-        
+
 //         // console.log(del.imagePath);
 
-//         fs.unlink( `/home/kali/theBest/EducationMERN/backend/uploads/${del.imagePath}`, function (err) {            
-//             if (err) {                                                 
-//                 console.error(err);                                    
-//             }                                                          
-//            console.log('File has been Deleted');                           
-//         }); 
+//         fs.unlink( `/home/kali/theBest/EducationMERN/backend/uploads/${del.imagePath}`, function (err) {
+//             if (err) {
+//                 console.error(err);
+//             }
+//            console.log('File has been Deleted');
+//         });
 //         let image =req.file.originalname
 //         let category = ({
-            
+
 //             name:req.body.name,
 //             surName:req.body.surName,
 //             address:req.body.address,
@@ -74,41 +72,27 @@ async function addPress(req,res) {
 //             subject:req.body.subject
 //         })
 //         let result=await PupilsModel.findByIdAndUpdate(userId,category)
-      
+
 //         return res.status(200).send(result)
 //     }catch(err){
 //         res.status(404).send(err)
 //     }
 // }
 
-// async function deletePupils(req,res) {
-//     try{
-        
-       
-//         let userId=req.params.id
-//         let del= await PupilsModel.findOne({userId})
-        
-//         console.log(del.imagePath);
+async function deletePress(req, res) {
+  try {
+    let userId = req.params.id;
+    let del = await PressModel.findOne({ _id: userId });
 
-//         fs.unlink( `/home/kali/theBest/EducationMERN/backend/uploads/${del.imagePath}`, function (err) {            
-//             if (err) {                                                 
-//                 console.error(err);                                    
-//             }                                                          
-//            console.log('File has been Deleted');                           
-//         }); 
+    let result = await PressModel.findByIdAndDelete(userId);
 
-//         let result=await PupilsModel.findByIdAndDelete(userId)
-//         // console.log(del.imagePath); 
-//         // fs.unlinkSync();
-      
+    return res.status(200).send(result);
+  } catch (err) {
+    res.status(400).send(err);
+  }
+}
 
-//         return res.status(200).send(result)
-//     }catch(err){
-//         res.status(400).send(err)
-//     }
-// }
-
-// // to`lov uchun 
+// // to`lov uchun
 // async function payPupils(req,res){
 //     try{
 //         let userId=req.params.id
@@ -127,10 +111,10 @@ async function addPress(req,res) {
 //     }
 // }
 
-module.exports={
-    getPress,
-    addPress
-    // updatePupils,
-    // deletePupils,
-    // payPupils
-}
+module.exports = {
+  getPress,
+  addPress,
+  // updatePupils,
+  deletePress,
+  // payPupils
+};
--- press/press.router.js
+++ press/press.router.js
@@ -5,7 +5,7 @@ const press=require('./press')
 router.route('/').get(press.getPress)
 router.route('/').post(press.addPress)
 // router.route('/:id').put(press.updatepress)
-// router.route('/:id').delete(press.deletepress)
+router.route('/:id').delete(press.deletePress)
 // router.route('/pay/:id').put(press.paypress)
 
 
